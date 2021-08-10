2f46e84e808bf3279dbc6e560b81b82cbd631a74
kevinorh <kevinorh@hotmail.com>
2021-08-10 11:31:16 -0500

    first commit

---
+++ LecturaYEscritura/UseCases/Home/HomeController.cs
@@ -0,0 +1,157 @@
+﻿using LecturaYEscritura.UseCases.Home;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LecturaYEscritura.UseCases
+{
+    public class HomeController : Controller
+    {
+        private static string baseUrl = "https://l-srvpivision/piwebapi/";
+        private class PIWebAPIInput
+        {
+            public string Timestamp { get; set; }
+            public double Value { get; set; }
+            public string UnitsAbbreviation { get; set; } = "";
+            public bool Good { get; set; } = true;
+            public bool Questionable { get; set; } = true;
+            public PIWebAPIInput()
+            {
+            }
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> GetData()
+        {
+            dynamic response;
+            var process = new List<string>();
+            string userName = @"cmbsaa\l-usrpiadmin";
+            string password = "Limaabril2021$";
+            //string tagPath = tagTextBox.Text;
+            string webId = "F1DP8mfxL4ZDU0K2ynkbula9xQBAIAAATC1TUlZQSURBXFBSVUVCQVdFQkFQSQ";
+            PIWebApiClient piWebAPIClient = new PIWebApiClient(userName, password);
+            try
+            {
+                //Resolve tag path
+                string requestUrl = baseUrl + "/streams/" + webId + "/plot";
+                string tget = await piWebAPIClient.GetAsync(requestUrl);
+                process.Add("Processing...");
+                response = new
+                {
+                    success = true,
+                    process = JsonConvert.SerializeObject(process),
+                    data = tget
+                };
+            }
+            catch (HttpRequestException ex)
+            {
+                process.Add(ex.Message);
+                response = new
+                {
+                    success = false,
+                    process = JsonConvert.SerializeObject(process)
+                };
+            }
+            catch (Exception ex)
+            {
+                process.Add(ex.Message); 
+                response = new
+                {
+                    success = false,
+                    process = JsonConvert.SerializeObject(process)
+                };
+            }
+            finally
+            {
+                //We are closing the HttpClient after every write in this simple example. This is not necessary.
+                piWebAPIClient.Dispose();
+            }
+            
+            return Ok(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> PostData(string webId, string fecha,string hora,int valor)
+        {
+            dynamic response;
+            var process = new List<string>();
+            string userName = @"cmbsaa\l-usrpiadmin";
+            string password = "Limaabril2021$";
+            //string tagPath = tagTextBox.Text;
+            webId = "F1DP8mfxL4ZDU0K2ynkbula9xQBAIAAATC1TUlZQSURBXFBSVUVCQVdFQkFQSQ";
+            PIWebApiClient piWebAPIClient = new PIWebApiClient(userName, password);
+            try
+            {
+                //Convert Date and Time to TimeSpan 2021-08-03T21:07:08Z
+                DateTime toTimeSpan = DateTime.Now;
+                string timeSpan;
+                if(DateTime.TryParse(fecha + " " + hora, out toTimeSpan))
+                {
+                    //DateTime.Now.ToString("yyyy’-‘MM’-‘dd’T’HH’:’mm’:’ss.fffffffK")
+                    timeSpan = toTimeSpan.ToString("yyyy-MM-ddTHH:mm:ssZ");
+                }
+                else
+                {
+                    timeSpan = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ");
+                }
+                var input = new PIWebAPIInput
+                {
+                    Timestamp = timeSpan,
+                    Value = valor
+                };
+                //Resolve tag path
+                string requestUrl = baseUrl + "/streams/" + webId + "/value";
+                await piWebAPIClient.PostAsync(requestUrl,JsonConvert.SerializeObject(input));
+                process.Add("Processing...");
+                response = new
+                {
+                    success = true,
+                    process = JsonConvert.SerializeObject(process)
+                };
+            }
+            catch (HttpRequestException ex)
+            {
+                process.Add(ex.Message);
+                response = new
+                {
+                    success = false,
+                    process = JsonConvert.SerializeObject(process)
+                };
+            }
+            catch (Exception ex)
+            {
+                process.Add(ex.Message); 
+                response = new
+                {
+                    success = false,
+                    process = JsonConvert.SerializeObject(process)
+                };
+            }
+            finally
+            {
+                //We are closing the HttpClient after every write in this simple example. This is not necessary.
+                piWebAPIClient.Dispose();
+            }
+            
+            return Ok(response);
+        }
+        /*
+        {
+            "Timestamp": "2021-08-03T21:07:08Z",
+            "Value": 10.0,
+            "UnitsAbbreviation": "",
+            "Good": true,
+            "Questionable": false
+        }
+         */
+    }
+}
---
+++ LecturaYEscritura/UseCases/Home/PIWebApiClient.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LecturaYEscritura.UseCases.Home
+{
+    public class PIWebApiClient
+    {
+        private HttpClient client;
+
+        /* Initiating HttpClient using the default credentials.
+         * This can be used with Kerberos authentication for PI Web API. */
+        public PIWebApiClient()
+        {
+            client = new HttpClient(new HttpClientHandler() { UseDefaultCredentials = true });
+        }
+
+        /* Initializing HttpClient by providing a username and password. The basic authentication header is added to the HttpClient.
+         * This can be used with Basic authentication for PI Web API. */
+        public PIWebApiClient(string userName, string password)
+        {
+            client = new HttpClient();
+            string authInfo = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(String.Format("{0}:{1}", userName, password)));
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", authInfo);
+        }
+
+        /* Disposing the HttpClient. */
+        public void Dispose()
+        {
+            client.Dispose();
+        }
+
+        /* Async GET request. This method makes a HTTP GET request to the uri provided
+         * and throws an exception if the response does not indicate a success. */
+        public async Task<string> GetAsync(string uri)
+        {
+            HttpResponseMessage response = await client.GetAsync(uri);
+            string content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                var responseMessage = "Response status code does not indicate success: " + (int)response.StatusCode + " (" + response.StatusCode + " ). ";
+                throw new HttpRequestException(responseMessage + Environment.NewLine + content);
+            }
+            return JsonConvert.SerializeObject(content);
+        }
+
+        /* Async POST request. This method makes a HTTP POST request to the uri provided
+         * and throws an exception if the response does not indicate a success. */
+        public async Task PostAsync(string uri, string data)
+        {
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));//ACCEPT header
+            client.DefaultRequestHeaders.Add("X-Requested-With", "message/http");
+            HttpResponseMessage response = await client.PostAsync(uri, new StringContent(data, Encoding.UTF8, "application/json"));
+            
+            string content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                var responseMessage = "Response status code does not indicate success: " + (int)response.StatusCode + " (" + response.StatusCode + " ). ";
+                throw new HttpRequestException(responseMessage + Environment.NewLine + content);
+            }
+        }
+        /* Calling the GetAsync method and waiting for the results. */
+        public string GetRequest(string url)
+        {
+            var t = this.GetAsync(url);
+            t.Wait();
+            return t.Result;
+        }
+
+        /* Calling the PostAsync method and waiting for the results. */
+        public void PostRequest(string url, string data)
+        {
+            Task t = this.PostAsync(url, data);
+            t.Wait();
+        }
+    }
+}
